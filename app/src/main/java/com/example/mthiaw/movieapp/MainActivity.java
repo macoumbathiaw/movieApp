d932273942e87bc8d044edd881f5ae09bfbfdb6f
Macoumba Thiaw <macoumbathiaw00@gmail.com>
2018-06-05 11:23:50 -0700

    item menu

--- app/src/main/java/com/example/mthiaw/movieapp/MainActivity.java
+++ app/src/main/java/com/example/mthiaw/movieapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mthiaw.movieapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +14,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Inflating the menu item
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+    //Do something when item is selected
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        int menuItemSelected = item.getItemId();
+        if (menuItemSelected == R.id.filter_menu){
+            Toast.makeText(this,"Filter Item Selected",Toast.LENGTH_SHORT).show();
+        }
+        return super.onContextItemSelected(item);
     }
 }
